913c3bc35083c53450cf7237ac16a7d88008b6a5
andreafspeziale <andreafspeziale@MacBook-Air-di-Andrea-2.local>
2015-06-16 10:06:34 +0200

    added comments

--- js/directives/appInfo.js
+++ js/directives/appInfo.js
@@ -1,9 +1,22 @@
 app.directive('appInfo', function() { 
-  return { 
-    restrict: 'E', 
+  return {
+  	/*
+		Restrict specifies how the directive will be used in the view. 
+		The 'E' means it will be used as a new HTML element.
+  	*/ 
+    restrict: 'E',
+    /*
+		Scope specifies that we will pass information into this 
+		directive through an attribute named info. 
+		The = tells the directive to look for an attribute named info in the <app-info> element.
+		The data in info becomes available to use in the template given by templateURL.
+    */ 
     scope: { 
       info: '=' 
-    }, 
+    },
+    /*
+		TemplateUrl specifies the HTML to use in order to display the data in scope.info
+    */ 
     templateUrl: 'js/directives/templates/appInfo.html' 
   }; 
 });
--- js/directives/installApp.js
+++ js/directives/installApp.js
@@ -4,8 +4,23 @@ app.directive('installApp', function() {
     		scope: {},
     		templateUrl: 'js/directives/templates/installApp.html',
     		/*
-    		The link is used to create interactive directives that respond to user actions. 
+    		  The link is used to create interactive directives 
+          that respond to user actions. 
     		*/
+
+        /*
+          Scope refers to the directive's scope.
+          Any new properties attached to $scope will become 
+          available to use in the directive's template.
+        */
+
+        /*
+          Element refers to the directive's HTML element.
+        */
+
+        /*
+          Attrs contains the element's attributes.
+        */
     		link: function(scope, element, attrs) {
     			scope.buttonText = "Install",
       			scope.installed = false,
